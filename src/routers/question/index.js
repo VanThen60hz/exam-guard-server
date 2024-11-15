0e8eadc191e26358bb20fa0902e9cfa125ebd706
Văn Thén <nguyenvthang2409@gmail.com>
2024-11-15 21:20:43 +0700

    Dev (#16)
    * add join exam
    
    * update logic when create question

--- src/routers/question/index.js
+++ src/routers/question/index.js
@@ -5,13 +5,14 @@ const { asyncHandler } = require("../../helpers/asyncHandler");
 const { teacherAuthentication, authentication } = require("../../auth/authUtils");
 const questionController = require("../../controllers/question.controller");
 const router = express.Router();
-const checkExamSubmission = require("../../utils/checkExamSubmission");
 
 // question router for user
-router.get("/:examId/list", authentication, checkExamSubmission, asyncHandler(questionController.listQuestions));
-router.get("/:examId/search", authentication, checkExamSubmission, asyncHandler(questionController.searchQuestions));
 
 router.use(teacherAuthentication);
+
+router.get("/:examId/list", asyncHandler(questionController.listQuestions));
+
+router.get("/:examId/search", asyncHandler(questionController.searchQuestions));
 
 // question router for teacher
 router.post("/:examId/create", asyncHandler(questionController.createQuestion));
